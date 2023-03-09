11a69d1829c38baa2031cedfe73146ef0ca9206e
Bryce Mecum <petridish@gmail.com>
2023-03-08 18:04:11 -0900

    Update load script to deal with BIT(1) fields

--- scripts/load.sh
+++ scripts/load.sh
@@ -7,25 +7,30 @@ mysql -u $AUTH --password=$AUTH -B -e "CREATE DATABASE ace_auth; CREATE DATABASE
 
 find DatabaseSetupScripts/Base -iname "*.sql" | while read f
 do
+  sed -i "s/bit(1)/tinyint(1)/g" "$f"
   mysql -u $AUTH --password=$AUTH < "$f";
 done;
 
 find DatabaseSetupScripts/Updates/Shard -iname "*.sql" | while read f
 do
+  sed -i "s/bit(1)/tinyint(1)/g" "$f"
   mysql --database=ace_shard -u $AUTH --password=$AUTH < "$f";
 done;
 
 find DatabaseSetupScripts/Updates/Authentication -iname "*.sql" | while read f
 do
+  sed -i "s/bit(1)/tinyint(1)/g" "$f"
   mysql --database=ace_auth -u $AUTH --password=$AUTH < "$f";
 done;
 
 find DatabaseSetupScripts/Updates/World -iname "*.sql" | while read f
 do
+  sed -i "s/bit(1)/tinyint(1)/g" "$f"
   mysql --database=ace_world -u $AUTH --password=$AUTH < "$f";
 done;
 
 find . -iname "ACE-World*.sql" | while read f
 do
+  sed -i "s/bit(1)/tinyint(1)/g" "$f"
   mysql --database=ace_world -u $AUTH --password=$AUTH < "$f";
 done;
